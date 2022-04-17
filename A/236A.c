7b9f058aeb0ce43611ae135dab60d557314c4291
trojanshakib <hello@shakib.com.bd>
2022-04-17 23:37:50 +0600

    236A

---
+++ A/236A.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char name[100];
+    scanf("%s", name);
+    int c = 0, dc, len = strlen(name);
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (name[i] == name[j])
+            {
+                c++;
+                break;
+            }
+        }
+    }
+    if ((len-c) % 2 == 0)
+    {
+        printf("CHAT WITH HER!\n");
+    }
+    else
+    {
+        printf("IGNORE HIM!\n");
+    }
+    return 0;
+}
