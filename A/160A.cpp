85032f002b7ea4a92a886653c816c74d1241cf90
trojanshakib <hello@shakib.com.bd>
2022-06-19 19:39:43 +0600

    CodeForces 160A

--- A/160A.cpp
+++ A/160A.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int main () {
-    
+int main()
+{
+    int n, total = 0, sum = 0, greedy = 0;
+    cin >> n;
+    int coins[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> coins[i];
+        total += coins[i];
+    }
+    sort(coins, coins + n, greater<int>());
+    for (int i = 0; i < n; i++)
+    {
+        if (sum > total / 2)
+        {
+            break;
+        }
+        sum += coins[i];
+        greedy++;
+    }
+    cout << greedy << endl;
+    return 0;
 }
